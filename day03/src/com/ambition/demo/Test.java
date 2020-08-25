79837b9e9a642b7a2a360c588bccd7581485232b
ambition <2576306698@qq.com>
2020-08-25 08:33:13 +0800

    node md

--- day03/src/com/ambition/demo/Test.java
+++ day03/src/com/ambition/demo/Test.java
@@ -2,6 +2,8 @@ package com.ambition.demo;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println(111);
+        System.out.println("hello\tjava");
+        System.out.println("hello\rjava");
+        System.out.println("hello\njava");
     }
 }
